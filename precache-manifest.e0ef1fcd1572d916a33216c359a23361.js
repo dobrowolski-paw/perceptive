561f9c1f990ad6c4402efdc72f9a11515a34400d
Paweł Dobrowolski <pawwel3@gmail.com>
2020-05-19 14:30:23 +0200

    Updates

---
+++ precache-manifest.e0ef1fcd1572d916a33216c359a23361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0179bdd37d4bccc8a1bb8fa78cf935",
+    "url": "/perceptivity/index.html"
+  },
+  {
+    "revision": "61e863c850f79321e559",
+    "url": "/perceptivity/static/css/main.42b359fb.chunk.css"
+  },
+  {
+    "revision": "c6b57830de7ddd1c0858",
+    "url": "/perceptivity/static/js/2.e2137b27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perceptivity/static/js/2.e2137b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e863c850f79321e559",
+    "url": "/perceptivity/static/js/main.c5be6d51.chunk.js"
+  },
+  {
+    "revision": "ec5b14bbc138c590a593",
+    "url": "/perceptivity/static/js/runtime-main.704667fd.js"
+  },
+  {
+    "revision": "40c4a10179c386e53a0b0d2992371f97",
+    "url": "/perceptivity/static/media/kogut.40c4a101.mp3"
+  }
+]);
